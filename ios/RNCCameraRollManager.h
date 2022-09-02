b7859b06264c3206fbb939bb0765b942b2c82921
oTranThanhNghia <thanhnghiaglhn92@gmail.com>
2022-09-02 15:23:14 +0700

    remove Apis (related to NSPhotoLibraryUsageDescription)

--- ios/RNCCameraRollManager.h
+++ ios/RNCCameraRollManager.h
@@ -5,18 +5,18 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-#import <Photos/Photos.h>
+//#import <Photos/Photos.h>
 
 #import <React/RCTBridgeModule.h>
 #import <React/RCTConvert.h>
 
-@interface RCTConvert (PHFetchOptions)
-
-+ (PHFetchOptions *)PHFetchOptionsFromMediaType:(NSString *)mediaType
-                                       fromTime:(NSUInteger)fromTime
-                                         toTime:(NSUInteger)toTime;
-
-@end
+//@interface RCTConvert (PHFetchOptions)
+//
+//+ (PHFetchOptions *)PHFetchOptionsFromMediaType:(NSString *)mediaType
+//                                       fromTime:(NSUInteger)fromTime
+//                                         toTime:(NSUInteger)toTime;
+//
+//@end
 
 
 @interface RNCCameraRollManager : NSObject <RCTBridgeModule>
